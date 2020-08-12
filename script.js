79119e67bc7a7939d9246293ebfddf097325d6ab
dieppthe <theo76.dieppois@gmail.com>
2020-08-12 11:18:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".panel");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    // Scroll plus naturel.
+    end: () => "+=" + document.querySelector(".container").offsetWidth
+  }
+});
